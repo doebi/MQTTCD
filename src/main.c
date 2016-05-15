b7b9f37fdfd980a2db80cef45ec97843799917ae
doebi <christoph@doeberl.at>
2016-05-15 19:30:57 +0200

    MQTT skeleton

--- src/main.c
+++ src/main.c
@@ -3,10 +3,51 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <syslog.h>
+#include "MQTTClient.h"
 //#include <stdio.h>
 //#include <fcntl.h>
 //#include <errno.h>
 //#include <string.h>
+
+#define ADDRESS     "tcp://mqtt.devlol.org:1883"
+#define CLIENTID    "user@notebook"
+#define TOPIC       "#"
+#define QOS         1
+#define TIMEOUT     10000L
+
+volatile MQTTClient_deliveryToken deliveredtoken;
+
+void delivered(void *context, MQTTClient_deliveryToken dt)
+{
+    printf("Message with token value %d delivery confirmed\n", dt);
+    deliveredtoken = dt;
+}
+
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
+{
+    int i;
+    char* payloadptr;
+
+    printf("Message arrived\n");
+    printf("     topic: %s\n", topicName);
+    printf("   message: ");
+
+    payloadptr = message->payload;
+    for(i=0; i<message->payloadlen; i++)
+    {
+        putchar(*payloadptr++);
+    }
+    putchar('\n');
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    return 1;
+}
+
+void connlost(void *context, char *cause)
+{
+    printf("\nConnection lost\n");
+    printf("     cause: %s\n", cause);
+}
 
 int main(void) {
     /* Our process ID and Session ID */
@@ -45,6 +86,24 @@ int main(void) {
     close(STDERR_FILENO);
 
     /* Daemon-specific initialization goes here */
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    int rc;
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+
+    MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
+
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        syslog (LOG_NOTICE, "Failed to connect, return code %d\n", rc);
+        exit(-1);
+    }
+    syslog (LOG_NOTICE, "Subscribing to topic %s\nfor client %s using QoS%d\n\n"
+           "Press Q<Enter> to quit\n\n", TOPIC, CLIENTID, QOS);
+    MQTTClient_subscribe(client, TOPIC, QOS);
 
     /* The Big Loop */
     while (1) {
